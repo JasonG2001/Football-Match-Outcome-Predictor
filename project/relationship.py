0272be8ec055ac4f773b59ec589a364fff31806f
jason <xiaohemc2001@gmail.com>
2022-09-09 13:14:03 +0100

    Work with generator

--- project/relationship.py
+++ project/relationship.py
@@ -3,25 +3,18 @@ import pandas as pd
 
 class ResultFinder:
 
-    def get_football_league(self, path):
+    def get_football_leagues(self, path) -> list:
+
+        #football_leagues: list[str] = [] 
 
         for football_league in os.listdir(path):
-            
-            return football_league
-        
-
-df = pd.read_csv(r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results/2_liga/Results_1990_2_liga.csv")
-home_team = set(df["Home_Team"])
-away_team = set(df["Away_Team"])
-home_team.update(away_team)
-
-print(len(home_team))
+            print(football_league)
+            yield football_league
 
 if __name__ == "__main__":
 
     result_finder = ResultFinder()
-    result_finder.get_football_league(r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results")
-
-
+    result_finder.get_football_leagues(r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results")
+    
 
 
