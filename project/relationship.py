4522f198476fa270ab41aa574cc700e146d7db29
jason <xiaohemc2001@gmail.com>
2022-09-09 14:49:14 +0100

    Add method to go to the directory of the football league

--- project/relationship.py
+++ project/relationship.py
@@ -3,19 +3,26 @@ import pandas as pd
 
 class ResultFinder:
 
-    def get_football_leagues(self, path) -> list:
+    def go_to_football_league(self, football_league: str) -> None:
 
-        football_leagues: list[str] = [] 
+        try:
 
-        for football_league in os.listdir(path):
-            
-            football_leagues.append(football_league)
+            os.chdir(fr"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results/{football_league}")
 
-        return football_leagues
+        except FileNotFoundError:
+
+            print("This football league results cannot be found, please check the spelling.")
+
+    def get_results(self, football_league: str, year: int):
+
+        self.go_to_football_league(football_league)
+
+
 
 if __name__ == "__main__":
 
+    PATH = r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results"
     result_finder = ResultFinder()
-    result_finder.get_football_leagues(r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results")
+    result_finder.go_to_football_league("2_lig")
 
 
