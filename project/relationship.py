6a05c2c75a3f20f6ad832643fb692dc805095e3c
jason <xiaohemc2001@gmail.com>
2022-09-09 21:19:19 +0100

    Add get_result method

--- project/relationship.py
+++ project/relationship.py
@@ -1,5 +1,4 @@
 import os
-import pandas as pd
 
 class ResultFinder:
 
@@ -13,16 +12,25 @@ class ResultFinder:
 
             print("This football league results cannot be found, please check the spelling.")
 
-    def get_results(self, football_league: str, year: int):
+    def get_results(self, football_league: str, year: int) -> str:
 
         self.go_to_football_league(football_league)
 
+        list_of_results: list[str] = os.listdir(fr"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results/{football_league}")
+        index_of_thousands: int = 8
+        index_of_ones: int = 12
+
+        for annual_result in list_of_results:
+
+            if str(year) == annual_result[index_of_thousands:index_of_ones]:
+                
+                return annual_result
 
 
 if __name__ == "__main__":
 
     PATH = r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results"
     result_finder = ResultFinder()
-    result_finder.go_to_football_league("2_lig")
+    result_finder.get_results("ligue_2", 2000)
 
 
