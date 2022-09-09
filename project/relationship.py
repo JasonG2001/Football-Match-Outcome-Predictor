f9561bd02c3a9f9aa4ab8c0627a580ddbe466282
jason <xiaohemc2001@gmail.com>
2022-09-09 12:34:55 +0100

    Add result finder

--- project/relationship.py
+++ project/relationship.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 class ResultFinder:
 
-    def __init__(self, path):
+    def get_football_league(self, path):
 
-        self.path = path
+        for football_league in os.listdir(path):
+            
+            return football_league
         
-    def get_foot_league(self):
-        pass
 
 df = pd.read_csv(r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results/2_liga/Results_1990_2_liga.csv")
 home_team = set(df["Home_Team"])
@@ -16,6 +16,11 @@ away_team = set(df["Away_Team"])
 home_team.update(away_team)
 
 print(len(home_team))
+
+if __name__ == "__main__":
+
+    result_finder = ResultFinder()
+    result_finder.get_football_league(r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results")
 
 
 
