95cd9841366f554d85187b13c44b7d1e34742a25
jason <xiaohemc2001@gmail.com>
2022-09-09 14:19:34 +0100

    Add method to get list of football leagues

--- project/relationship.py
+++ project/relationship.py
@@ -5,16 +5,17 @@ class ResultFinder:
 
     def get_football_leagues(self, path) -> list:
 
-        #football_leagues: list[str] = [] 
+        football_leagues: list[str] = [] 
 
         for football_league in os.listdir(path):
-            print(football_league)
-            yield football_league
+            
+            football_leagues.append(football_league)
+
+        return football_leagues
 
 if __name__ == "__main__":
 
     result_finder = ResultFinder()
     result_finder.get_football_leagues(r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results")
-    
 
 
