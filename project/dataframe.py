cf484c6750c7625d0166fa774d00a58d71807a65
jason <xiaohemc2001@gmail.com>
2022-09-10 13:28:05 +0100

    Add method to list all teams over all years

--- project/dataframe.py
+++ project/dataframe.py
@@ -1,4 +1,5 @@
 from result_finder import ResultFinder
+import os
 import pandas as pd
 
 class DataframeAnalysis:
@@ -15,11 +16,33 @@ class DataframeAnalysis:
 
             df = pd.read_csv(fr"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results/{football_league}/{result}")
 
-            print(df)
+            return df
 
         except FileNotFoundError:
 
             print("The football league or annual results cannot be found. Please check the league name is correct or year is correct.")
+
+    def get_list_of_teams(self, football_league, year=None):
+        
+        if year == None: # Gets list of teams in the whole league over all years
+            
+            self.result_finder.go_to_football_league(football_league)
+            
+            annual_results: list[str] = os.listdir(".")
+
+            list_of_teams = []
+
+            for annual_result in annual_results:
+
+                df = pd.read_csv(annual_result)
+
+                teams = list(df["Home_Team"])
+
+                list_of_teams.extend(teams)
+
+            print(set(list_of_teams))
+
+            
 
     
 
@@ -27,4 +50,4 @@ class DataframeAnalysis:
 if __name__ == "__main__":
 
     dataframe = DataframeAnalysis()
-    dataframe.get_dataframe("premier_league", 20)
+    dataframe.get_list_of_teams("premier_league")
