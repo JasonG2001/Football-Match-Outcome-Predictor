06e1615ba994069f17e43813108fcc11d0a66bf7
jason <xiaohemc2001@gmail.com>
2022-09-12 23:28:29 +0100

    Add method to retrieve the list of years with results

--- project/result_finder.py
+++ project/result_finder.py
@@ -2,6 +2,11 @@ import os
 
 class ResultFinder:
 
+    def __init__(self):
+
+        self.INDEX_OF_THOUSANDS: int = 8
+        self.INDEX_OF_ONES: int = 12
+
     def go_to_football_league(self, football_league: str) -> None:
 
         try:
@@ -17,21 +22,38 @@ class ResultFinder:
         self.go_to_football_league(football_league)
 
         list_of_results: list[str] = os.listdir(fr"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results/{football_league}")
-        index_of_thousands: int = 8
-        index_of_ones: int = 12
 
         for annual_result in list_of_results:
 
-            if str(year) == annual_result[index_of_thousands:index_of_ones]:
+            if str(year) == annual_result[self.INDEX_OF_THOUSANDS:self.INDEX_OF_ONES]:
                 
                 return annual_result
+
+            
+    def get_list_of_years(self, football_league: str):
+        
+        self.go_to_football_league(football_league)
+
+        list_of_results: list[str] = os.listdir(fr"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results/{football_league}")
+
+        years: list[int] = []
+
+        for result in list_of_results:
+
+            year = int(result[self.INDEX_OF_THOUSANDS:self.INDEX_OF_ONES])
+
+            years.append(year)
+
+        return years
+
+
+
 
 
 if __name__ == "__main__":
 
-    PATH = r"/home/jason2001/Football-Match-Outcome-Predictor/project/Football/Results"
     result_finder = ResultFinder()
-    result_finder.get_results("ligue_2", 200)
+    print(result_finder.get_list_of_years("ligue_2"))
 
 
  
