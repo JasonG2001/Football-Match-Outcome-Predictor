0b5b3401311a2fe9ba51fdeb007dc1a2a9581aee
jason <xiaohemc2001@gmail.com>
2022-09-24 21:19:45 +0100

    Add method to make column in dataframe with total wins

--- project/football_dataframe.py
+++ project/football_dataframe.py
@@ -9,11 +9,19 @@ class FootballDataframe:
         self.dataframe = DataframeAnalysis(football_league)
 
 
-    def make_dataframe(self):
+    def make_dataframe(self, *args):
 
         indexed_teams: dict[int:str] = self.dataframe.get_teams_indexed()
 
-        return pd.DataFrame.from_dict(indexed_teams, orient="index")
+        df = pd.DataFrame.from_dict(indexed_teams, orient="index")
+
+        win_board: dict[str:int] = self.dataframe.get_win_board_over_all_years()
+
+        wins = list(win_board.values())
+
+        df["Wins"] = wins
+
+        return df
 
 
 if __name__ == "__main__":
