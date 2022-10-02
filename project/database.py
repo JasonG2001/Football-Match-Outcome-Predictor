a973800494603015a8fdbc959d0ac5efd47e6ada
jason <xiaohemc2001@gmail.com>
2022-10-02 18:54:06 +0100

    Add method to make postgres table

--- project/database.py
+++ project/database.py
@@ -14,6 +14,22 @@ class Database:
             with conn.cursor() as cur:
                 cur.execute(sql_code)
 
+
+    def create_table(self, host: str, user: str, password: str, dbname: str, port: int):
+
+        sql_code: str = """
+        DROP TABLE IF EXISTS football_data; CREATE
+        TABLE football_data (
+            teams       VARCHAR,
+            wins        INTEGER,
+            streaks     INTEGER,
+            goals       INTEGER
+        );
+        
+        """
+        
+        self.execute_to_postgres(host, user, password, dbname, port, sql_code)
+
     
     def give_sql_input(self):
 
@@ -29,6 +45,7 @@ if __name__ == "__main__":
     PORT: int = 5432
 
     database = Database("premier_league")
+    database.create_table(HOST, USER, PASSWORD, DBNAME, PORT)
 
     
 
