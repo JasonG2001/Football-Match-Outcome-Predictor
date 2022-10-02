2fba4373aace2834d0af22b9da670d083a5edb05
jason <xiaohemc2001@gmail.com>
2022-10-02 14:41:38 +0100

    Add method to pass sql code

--- project/database.py
+++ project/database.py
@@ -1,19 +1,35 @@
 from football_dataframe import FootballDataframe
 import psycopg2
 
-class database:
+class Database:
 
     def __init__(self, football_league: str):
 
         self.dataframe = FootballDataframe(football_league)
 
     
-    def connect_to_rds(self, host: str, user: str, password: str, dbname: str, port: int):
+    def execute_to_postgres(self, host: str, user: str, password: str, dbname: str, port: int, sql_code: str):
 
-        pass
+        with psycopg2.connect(host = host, user = user, password = password, dbname = dbname, port = port) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql_code)
+
     
     def give_sql_input(self):
 
         pass
+
+
+if __name__ == "__main__":
+
+    HOST: str = "football.cqav9sfxcwg5.eu-west-2.rds.amazonaws.com"
+    USER: str = "postgres"
+    PASSWORD: str = "Jguan2001"
+    DBNAME: str = "postgres"
+    PORT: int = 5432
+
+    database = Database("premier_league")
+
+    
 
     
