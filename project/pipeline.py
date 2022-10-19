b24f0c1f74c2a0182e913539750c5a835ffe9d1b
jason <xiaohemc2001@gmail.com>
2022-10-19 23:43:48 +0100

    Import modules

---
+++ project/pipeline.py
@@ -0,0 +1,5 @@
+from football_dataframe import FootballDataframe
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
