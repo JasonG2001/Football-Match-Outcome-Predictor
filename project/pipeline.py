5fbb2bfc4f9937cf2555d16d6d1e181a5b876943
jason <xiaohemc2001@gmail.com>
2022-10-20 23:07:55 +0100

    Fit model

--- project/pipeline.py
+++ project/pipeline.py
@@ -23,18 +23,29 @@ class Model:
         return pipe
 
 
-    def temp(self):    
+    def get_training_and_testing_set(self):    
 
         df = self.football_dataframe.make_dataframe()
 
         X = df[["Wins", "Streaks", "Goals"]]
         y = df["Goals"]
 
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
+        return X_train, X_test, y_train, y_test
+
+
+    def fit_model(self):
+
+        pipe = self.make_pipeline()
+        X_train, X_test, y_train, y_test = self.get_training_and_testing_set()
+
+        pipe.fit(X_train, y_train)
 
 
 if __name__ == "__main__":
 
     model = Model("premier_league")
-    print(model.make_pipeline())
+    # print(model.get_training_and_testing_set())
+    print(model.get_training_and_testing_set())
 
