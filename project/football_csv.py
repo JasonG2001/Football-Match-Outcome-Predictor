e7b51c4283753c8c7a05e33c98d9d76058a91a64
jason <xiaohemc2001@gmail.com>
2022-11-24 00:20:40 +0000

    Clean the dataset

--- project/football_csv.py
+++ project/football_csv.py
@@ -6,15 +6,26 @@ class FootballCSV:
 
         self.football_dataframe = FootballDataframe(football_league)
 
+    
+    def clean_dataset(self, year: int):
 
-    def convert_to_csv(self):
+        df = self.football_dataframe.make_dataframe(year)
+        df["Home_Team_Code"] = df["Home_Team"].astype("category").cat.codes
+        df["Away_Team_Code"] = df["Away_Team"].astype("category").cat.codes
+        df["Home_Result_Code"] = df["Home_Result"].astype("category").cat.codes
+        df["Away_Result_Code"] = df["Away_Result"].astype("category").cat.codes
 
-        df = self.football_dataframe.make_dataframe()
+        return df
+
+
+    def convert_to_csv(self, year: int):
+
+        df = self.clean_dataset(year)
 
         return df.to_csv(r"/home/jason2001/Football-Match-Outcome-Predictor/project/cleaned_dataset.csv")
 
 
 if __name__ == "__main__":
 
-    csv = FootballCSV("championship")
+    csv = FootballCSV("premier_league")
     csv.convert_to_csv()
