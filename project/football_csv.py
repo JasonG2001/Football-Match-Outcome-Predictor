29d496f16bad1829382c36a2ad9bdaf1a67bb7d2
jason <xiaohemc2001@gmail.com>
2022-11-24 13:25:58 +0000

    Make football csv for specified league and year

--- project/football_csv.py
+++ project/football_csv.py
@@ -1,31 +1,21 @@
 from football_dataframe import FootballDataframe
 
-class FootballCSV:
+class MakeCSV:
 
     def __init__(self, football_league: str):
 
+        self.football_league: str = football_league 
         self.football_dataframe = FootballDataframe(football_league)
-
-    
-    def clean_dataset(self, year: int):
-
-        df = self.football_dataframe.make_dataframe(year)
-        df["Home_Team_Code"] = df["Home_Team"].astype("category").cat.codes
-        df["Away_Team_Code"] = df["Away_Team"].astype("category").cat.codes
-        df["Home_Result_Code"] = df["Home_Result"].astype("category").cat.codes
-        df["Away_Result_Code"] = df["Away_Result"].astype("category").cat.codes
-
-        return df
 
 
     def convert_to_csv(self, year: int):
 
-        df = self.clean_dataset(year)
+        df = self.football_dataframe.clean_dataframe(year)
 
-        return df.to_csv(r"/home/jason2001/Football-Match-Outcome-Predictor/project/cleaned_dataset.csv")
+        return df.to_csv(fr"/home/jason2001/Football-Match-Outcome-Predictor/project/{self.football_league}_{year}_cleaned_dataset.csv")
 
 
 if __name__ == "__main__":
 
-    csv = FootballCSV("premier_league")
-    csv.convert_to_csv()
+    csv = MakeCSV("premier_league")
+    csv.convert_to_csv(2021)
