aa1104567bfff2cd872979587a52b2131c60df30
jason <xiaohemc2001@gmail.com>
2022-12-19 00:55:58 +0000

    Refactor and annotate type

--- project/football_dataframe.py
+++ project/football_dataframe.py
@@ -1,56 +1,58 @@
 from dataframe import DataframeAnalysis
+from typing import List, Type
 import pandas as pd
 
 class FootballDataframe:
 
-    def __init__(self, football_league: str):
+    def __init__(self) -> None:
 
-        self.dataframe = DataframeAnalysis(football_league)
+        self.dataframe = DataframeAnalysis()
 
 
-    def make_dataframe(self, year: int):
+    def make_dataframe(self, football_league: str, year: int) -> Type[pd.DataFrame]:
 
-        df = self.dataframe.get_dataframe(year)
-        df = df.drop(["Result", "Link", "League"], axis=1)
+        df: Type[pd.DataFrame] = self.dataframe.get_dataframe(football_league, year)
+        df: Type[pd.DataFrame] = df.drop(["Result", "Link", "League"], axis=1)
 
-        home_elos, away_elos = self.dataframe.get_home_and_away_elos(year)
-        home_goals_so_far, away_goals_so_far = self.dataframe.get_home_and_away_goals_so_far(year)
-        home_wins_so_far, away_wins_so_far, home_losses_so_far, away_losses_so_far, home_draws_so_far, away_draws_so_far = self.dataframe.get_wins_losses_draws_so_far(year)
-        current_home_streak, current_away_streak = self.dataframe.get_current_streak(year)
-        home_results, away_results = self.dataframe.get_result(year)
+        home_elos, away_elos = self.dataframe.get_home_and_away_elos(football_league, year)
+        home_goals_so_far, away_goals_so_far = self.dataframe.get_home_and_away_goals_so_far(football_league, year)
+        home_wins_so_far, away_wins_so_far, home_losses_so_far, away_losses_so_far, home_draws_so_far, \
+            away_draws_so_far = self.dataframe.get_wins_losses_draws_so_far(football_league, year)
+        current_home_streak, current_away_streak = self.dataframe.get_current_streak(football_league, year)
+        home_results, away_results = self.dataframe.get_result(football_league, year)
 
-        df["Home_Elos"] = home_elos
-        df["Away_Elos"] = away_elos
-        df["Home_Goals"] = home_goals_so_far
-        df["Away_Goals"] = away_goals_so_far
-        df["Home_Wins"] = home_wins_so_far
-        df["Away_Wins"] = away_wins_so_far
-        df["Home_Losses"] = home_losses_so_far
-        df["Away_Losses"] = away_losses_so_far
-        df["Home_Draws"] = home_draws_so_far
-        df["Away_Draws"] = away_draws_so_far
-        df["Home_Streak"] = current_home_streak
-        df["Away_Streak"] = current_away_streak
+        df["Home_Elos"]: List[float] = home_elos
+        df["Away_Elos"]: List[float] = away_elos
+        df["Home_Goals"]: List[int] = home_goals_so_far
+        df["Away_Goals"]: List[int] = away_goals_so_far
+        df["Home_Wins"]: List[int] = home_wins_so_far
+        df["Away_Wins"]: List[int] = away_wins_so_far
+        df["Home_Losses"]: List[int] = home_losses_so_far
+        df["Away_Losses"]: List[int] = away_losses_so_far
+        df["Home_Draws"]: List[int] = home_draws_so_far
+        df["Away_Draws"]: List[int] = away_draws_so_far
+        df["Home_Streak"]: List[int] = current_home_streak
+        df["Away_Streak"]: List[int] = current_away_streak
 
-        df["Home_Result"] = home_results
-        df["Away_Result"] = away_results
+        df["Home_Result"]: List[str] = home_results
+        df["Away_Result"]: List[str] = away_results
 
         return df
 
 
-    def clean_dataframe(self, year: int):
+    def clean_dataframe(self, football_league: str, year: int) -> Type[pd.DataFrame]:
 
-        df = self.make_dataframe(year)
-        df["Home_Team_Code"] = df["Home_Team"].astype("category").cat.codes
-        df["Away_Team_Code"] = df["Away_Team"].astype("category").cat.codes
-        df["Home_Result_Code"] = df["Home_Result"].astype("category").cat.codes
-        df["Away_Result_Code"] = df["Away_Result"].astype("category").cat.codes
+        df: Type[pd.DataFrame] = self.make_dataframe(football_league, year)
+        df["Home_Team_Code"]: List[int] = df["Home_Team"].astype("category").cat.codes
+        df["Away_Team_Code"]: List[int] = df["Away_Team"].astype("category").cat.codes
+        df["Home_Result_Code"]: List[int] = df["Home_Result"].astype("category").cat.codes
+        df["Away_Result_Code"]: List[int] = df["Away_Result"].astype("category").cat.codes
 
         return df
 
 
 if __name__ == "__main__":
 
-    football_dataframe = FootballDataframe("premier_league")
-    print(football_dataframe.make_dataframe(2021))
-    print(football_dataframe.clean_dataframe(2021))
+    football_dataframe = FootballDataframe()
+    print(football_dataframe.make_dataframe("premier_league", 2021))
+    print(football_dataframe.clean_dataframe("premier_league", 2021))
