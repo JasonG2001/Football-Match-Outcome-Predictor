d1e5f1b0f7ddaefb80c28c62b2d959c7ffd3e963
jason <xiaohemc2001@gmail.com>
2022-12-30 12:14:51 +0000

    Make method to obtain cumulative wins

--- project/new_results.py
+++ project/new_results.py
@@ -56,7 +56,8 @@ class NewResult:
         df: Type[pd.DataFrame] = df.loc[:, ["Home_Team", "Away_Team", "Result"]]
         goals: Dict[str,int] = defaultdict(int)
 
-        for i, record in df.iterrows():
+        record: Type[pd.DataFrame]
+        for _, record in df.iterrows():
             home_team: str = record.loc["Home_Team"]
             away_team: str = record.loc["Away_Team"]
 
@@ -67,6 +68,51 @@ class NewResult:
             goals[away_team] += int(record.loc["Result"][self.INDEX_OF_AWAY_TEAM_SCORE])
 
         return cumulative_home_goals, cumulative_away_goals
+
+
+    def get_cumulative_home_and_away_wins(self, df: Type[pd.DataFrame]) -> List[int]:
+
+        cumulative_home_wins: List[int] = []
+        cumulative_away_wins: List[int] = []
+        cumulative_home_losses: List[int] = []
+        cumulative_away_losses: List[int] = []
+        cumulative_home_draws: List[int] = []
+        cumulative_away_draws: List[int] = []
+
+        df: Type[pd.DataFrame] = df.loc[:, ["Home_Team", "Away_Team", "Result"]]
+        wins: Dict[str,int] = defaultdict(int)
+        losses: Dict[str,int] = defaultdict(int)
+        draws: Dict[str,int] = defaultdict(int)
+
+        record: Type[pd.DataFrame]
+        for _, record in df.iterrows():
+            home_team: str = record.loc["Home_Team"]
+            away_team: str = record.loc["Away_Team"]
+
+            cumulative_home_wins.append(wins[home_team])
+            cumulative_away_wins.append(wins[away_team])
+            cumulative_home_losses.append(losses[home_team])
+            cumulative_away_losses.append(losses[away_team])
+            cumulative_home_draws.append(draws[home_team])
+            cumulative_away_draws.append(draws[away_team])
+
+            home_score: int = int(record.loc["Result"][self.INDEX_OF_HOME_TEAM_SCORE])
+            away_score: int = int(record.loc["Result"][self.INDEX_OF_AWAY_TEAM_SCORE])
+
+            if home_score > away_score:
+                wins[home_team] += 1
+                losses[away_team] += 1
+
+            elif home_score < away_score:
+                wins[away_team] += 1
+                losses[home_team] += 1
+
+            else: 
+                draws[home_team] += 1
+                draws[away_team] += 1
+
+        return cumulative_home_wins, cumulative_away_wins, cumulative_home_losses, cumulative_away_losses, \
+            cumulative_home_draws, cumulative_away_draws
 
 
     def save_to_csv(self, dataframe: Type[pd.DataFrame]) -> None:
